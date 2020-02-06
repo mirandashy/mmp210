e0507f82ff3c73dc5106f53056980e23da9fd3f1
mirandashy <60408940+mirandashy@users.noreply.github.com>
2020-02-06 17:41:46 -0500

    adds shapes to selfportrait

--- self_portrait/sketch.js
+++ self_portrait/sketch.js
@@ -1,12 +1,65 @@
+/* self portrait
+	by miranda
+	2/06/2020
+*/
+
+
 function setup () {
 	createCanvas(640, 360);
 }
 
 function draw () {
 	background ('orange');
-	ellipse(320, 180, 200);
 
-	ellipse (290, 160, 30);
+ 	// shapes with xy coordinate and size //
+ 	fill (255, 250, 98);
+ 	stroke ('red');
+ 	strokeWeight (4);
+	ellipse(320, 180, 200); // face
+	fill ("white");
+	ellipse (290, 160, 30); //left eye
+	noFill();
+	arc(283, 165, 35, 35, HALF_PI, PI);
 
-	ellipse (350, 160, 30);
-}
+
+	noFill();
+	arc(350, 153, 35, 35, 0, -PI, PI); // right eye
+	arc(350, 165, 35, 35, -PI, HALF_PI);
+
+	rectMode (CENTER);
+	line (312, 205, 332, 205) //mouth
+
+	fill (255, 250, 98)
+	arc (416, 180, 25, 40, -HALF_PI, HALF_PI) // right ear
+	arc (225, 180, 25, 40, HALF_PI, -HALF_PI)
+
+	// shapes that are only xy points //
+
+
+	//arrow 
+	stroke ('red');
+	strokeWeight (4);
+	line(350, 50, 350, 230);
+
+	line(320, 50, 350, 80); // lines on the left
+	line(320, 40, 350, 70);
+	line(320, 30, 350, 60);
+	line(340, 215, 350, 230) // line on the tip
+	
+
+
+
+	line(380, 50, 350, 80); //lines on the right
+	line(380, 40, 350, 70);
+	line(380, 30, 350, 60);
+	line(360, 215, 350, 230) //line on the tip
+
+
+
+
+
+	//body
+	fill ('red');
+	triangle (320, 281, 200, 360 , 440, 360);
+
+}	
