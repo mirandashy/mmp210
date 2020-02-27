a57992d93c825e47eafc7ae5f0db0d359f00387c
mirandashy <60408940+mirandashy@users.noreply.github.com>
2020-02-27 17:27:40 -0500

    add image

--- meme_one/sketch.js
+++ meme_one/sketch.js
@@ -3,6 +3,14 @@
 */
 
 
+// global scope
+var catImage;
+
+function preload() {
+	catImage = loadImage("cat.jpg");
+} 
+
+
 function setup () {
 	var canvas= createCanvas(500, 500);
 	canvas.drawingContext.miterLimit = 1;
@@ -12,20 +20,30 @@ function setup () {
 function draw () {
 	background('grey');
 
+	// draw the image
+	image(catImage, 0, 70, 512, 384); 
+	//image(catImage, 300, 100, 300,300); 
+
+	//var imageColor= get(mouseX, mouseY);
+	//fill (imageColor);
+	//noStroke();
+	//rect(mouseX, mouseY, 50, 50);
+
+	//tint (255, 0, 100);
 	textSize(100);
 	//fill("blue");
 	//stroke("plum");
 	//strokeWeight(5);
 	//textStyle(ITALIC);
-	textFont('Brush Script MT')
+	textFont('Lacquer')
 	//textAlign(LEFT, TOP);
 
-	var hello = "Hello World";
-	var length = hello.lenght;
-	var n = map(mouseX, 0, 500, 0, 10);
+	//var hello = "Hello World";
+	//var length = hello.lenght;
+	//var n = map(mouseX, 0, 500, 0, 10);
 
-	var displayString = hello.substring(0, n);
-	text(displayString, 250, height/2);
+	//var displayString = hello.substring(0, n);
+	//text(displayString, 250, height/2);
 	//var s= mouseX;
 	//textSize(s);
 
@@ -41,9 +59,9 @@ function draw () {
 	textStyle(NORMAL);
 	textAlign(CENTER, TOP);
 
-
-	var welcome = "Welcome to my website";
+	fill("white");
+	var welcome = "When life becomes to hard to handle";
 	var end= map(frameCount, 0, 200, 0, welcome.length);
-	text (welcome.substring (0, end), 250, 350);
+	text (welcome.substring (0, end), 270, 80, 200);
 }
 
