c43b854251a78a1e547ffb02b58a294dbe384b05
mirandashy <60408940+mirandashy@users.noreply.github.com>
2020-03-31 14:17:25 -0400

    edit loop pattern

--- pattern_one/sketch.js
+++ pattern_one/sketch.js
@@ -8,60 +8,51 @@ for (let i = 100; i >= 1; i--) {
 	3.25.2020
 */
 
-function setup(){
+function setup()  {
 	createCanvas (640,360);
 }
 
-function draw(){
+function draw() {
 	background ("white");
 
-	for (let x = 0; x <= width; x +=100){
-
-		fill('red');
-		noStroke();
-		rect(0, 0, width, height/6);
-
-		fill ('red');
-		ellipse (x, 110, 40);
-
-		fill ('white');
-		ellipse (x, 100, 50);
+	strokeWeight (4);
+	stroke('red');
 
 
-		fill ('black');
-		ellipse (x, 90, 45);
+	for (let x = 100; x <= width; x += 120) {
+		line(x, 50, x, 230); //body
 
-		
+		line(x, 80, x+30, 50); //first right
+		line(x, 70, x+30, 40);
+		line(x, 60, x+30, 30);
 
-		
+		line(x, 230, x+10, 215) //tip right
 
-	}
+		line(x-30, 50, x, 80) //first left
+		line(x-30, 40, x, 70);
+		line(x-30, 30, x, 60);
 
-	for (let x = 0; x <= width; x +=80){
-
-		fill('black');
-		noStroke();
-		rect(0, height/2, width, height/6);
-	}
-
-	for (let x = 0; x <= width; x +=80){
-
-		fill('black');
-		noStroke();
-		rect(0, height, width, height/6);
-
-		fill ('black');
-		ellipse (x, 260, 40);
-
-		fill ('white');
-		ellipse (x, 250, 50);
-
-
-		fill ('red');
-		ellipse (x, 240, 45);
-
-		
+		line(x-10, 215, x, 230); //tip left
 
 	}
+	
 
+	
+	for (let x = 50; x <= width; x += 120) {
+		line(x, 120, x, 300);
+
+		line(x, 290, x-30, 320); //left
+		line(x, 280, x-30, 310);
+		line(x, 270, x-30, 300);
+
+
+		line(x, 120, x+10, 135);// tip right
+
+		line(x, 290, x+30, 320) //first left
+		line(x, 280, x+30, 310);
+		line(x, 270, x+30, 300);
+
+		line(x-10, 135, x, 120); //tip left
+
+	}
 }
